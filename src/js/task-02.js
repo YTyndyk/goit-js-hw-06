8ea977d990e898c1697031bec983d6201213bc64
YTyndyk <114148643+YTyndyk@users.noreply.github.com>
2023-02-07 18:19:46 +0200

    done task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const listEl = ingredients.map(element =>{
+  const itemEl = document.createElement('li');
+itemEl.textContent = element;
+itemEl.classList.add('item');
+return itemEl;
+
+});
+console.log(listEl);
+const list = document.querySelector('ul');
+list.append(...listEl);
