2697b4fcb7476629e363108b11203f15a48058bf
YTyndyk <114148643+YTyndyk@users.noreply.github.com>
2023-02-08 15:46:57 +0200

    add styles for task 3

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,13 +1,11 @@
-
 const categoriesNumberEl = document.querySelector("#categories");
 console.log("Number of categories:", categoriesNumberEl.children.length);
 
-const arrayEl = document.querySelectorAll('li.item');
-arrayEl.forEach(element => {
-    
-const categoryEl = element.firstElementChild.textContent;
-console.log('Category:', categoryEl);
+const arrayEl = document.querySelectorAll("li.item");
+arrayEl.forEach((element) => {
+	const categoryEl = element.firstElementChild.textContent;
+	console.log("Category:", categoryEl);
 
-const numbersEl = element.querySelector('h2');
-console.log('Elements:', numbersEl.nextElementSibling.children.length);
+	const numbersEl = element.querySelector("h2");
+	console.log("Elements:", numbersEl.nextElementSibling.children.length);
 });
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,18 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+	"Potatoes",
+	"Mushrooms",
+	"Garlic",
+	"Tomatos",
+	"Herbs",
+	"Condiments",
 ];
-const listEl = ingredients.map(element =>{
-  const itemEl = document.createElement('li');
-itemEl.textContent = element;
-itemEl.classList.add('item');
-return itemEl;
-
+const listEl = ingredients.map((element) => {
+	const itemEl = document.createElement("li");
+	itemEl.textContent = element;
+	itemEl.classList.add("item");
+	return itemEl;
 });
 console.log(listEl);
-const list = document.querySelector('#ingredients');
+const list = document.querySelector("#ingredients");
 list.append(...listEl);
