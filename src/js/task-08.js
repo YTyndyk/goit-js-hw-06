c769b600c78d833ef029703e60f59036bb4a9d0a
YTyndyk <114148643+YTyndyk@users.noreply.github.com>
2023-02-21 15:32:28 +0200

    done task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+	event.preventDefault();
+
+	const formElements = event.currentTarget.elements;
+
+	const email = formElements.email.value;
+	const password = formElements.password.value;
+	const formData = { email, password };
+
+	if (email && password === "") {
+		alert("Заповнені не всі поля!");
+	} else {
+		console.log(formData);
+		form.reset();
+	}
+}
