b87d2b7de98b9f5d5d25ea2a35d3f1d4b7fd7241
YTyndyk <114148643+YTyndyk@users.noreply.github.com>
2023-02-22 13:41:50 +0200

    done task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,14 @@
+const btnChangeColor = document.querySelector(".change-color");
+const spanRef = document.querySelector(".color");
+const bodyRef = document.querySelector("body");
+
+btnChangeColor.addEventListener("click", onBtnClick);
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onBtnClick() {
+	bodyRef.style.backgroundColor = getRandomHexColor();
+	spanRef.textContent = bodyRef.style.backgroundColor;
 }
