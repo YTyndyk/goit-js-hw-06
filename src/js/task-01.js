f0375d97443204ce225a4290c2e6be2c896aff59
YTyndyk <114148643+YTyndyk@users.noreply.github.com>
2023-02-22 10:39:06 +0200

    add second variant task 1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,11 +1,23 @@
-const categoriesNumberEl = document.querySelector("#categories");
-console.log("Number of categories:", categoriesNumberEl.children.length);
+// const categoriesNumberEl = document.querySelector("#categories");
+// console.log("Number of categories:", categoriesNumberEl.children.length);
+
+// const arrayEl = document.querySelectorAll("li.item");
+// arrayEl.forEach((element) => {
+// 	const categoryEl = element.firstElementChild.textContent;
+// 	console.log("Category:", categoryEl);
+
+// 	const numbersEl = element.querySelector("h2");
+// 	console.log("Elements:", numbersEl.nextElementSibling.children.length);
+// });
+
+const numberOfCategories = document.querySelectorAll(".item");
+console.log("Number of categories:", numberOfCategories.length);
 
 const arrayEl = document.querySelectorAll("li.item");
 arrayEl.forEach((element) => {
 	const categoryEl = element.firstElementChild.textContent;
-	console.log("Category:", categoryEl);
+	const elementsEl = element.lastElementChild.children.length;
 
-	const numbersEl = element.querySelector("h2");
-	console.log("Elements:", numbersEl.nextElementSibling.children.length);
+	console.log("Category:", categoryEl);
+	console.log("Elements:", elementsEl);
 });
