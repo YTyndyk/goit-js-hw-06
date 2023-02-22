d2d4afc5012feb8ceb32a1ef4b85f5aacb448020
YTyndyk <114148643+YTyndyk@users.noreply.github.com>
2023-02-22 17:02:35 +0200

    done 10 task

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,31 @@
+const numberRef = document.querySelector("input");
+const btnCreate = document.querySelector("button[data-create]");
+const btnDestroy = document.querySelector("button[data-destroy]");
+const boxesEl = document.querySelector("#boxes");
+
+btnCreate.addEventListener("click", createBoxes);
+btnDestroy.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+	amount = numberRef.value;
+
+	const basicSize = 30;
+
+	for (let i = 0; i < amount; i += 1) {
+		const size = basicSize + i * 10;
+		const divEl = document.createElement("div");
+
+		divEl.style.cssText = `width: ${size}px; height: ${size}px`;
+		divEl.style.backgroundColor = getRandomHexColor();
+		boxesEl.append(divEl);
+	}
+}
+
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+	return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function destroyBoxes() {
+	boxesEl.textContent = "";
+	numberRef.value = null;
 }
